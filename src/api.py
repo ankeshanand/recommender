a8acd91295f738473f995aa3a96d0ea9ea9aa8b8
Ankesh Anand <ankeshanand1994@gmail.com>
2016-08-13 16:17:17 +0530

    Make a singleton class to avoid repeated computations

--- src/api.py
+++ src/api.py
@@ -1,16 +1,21 @@
-import pandas as pd
+import json
 
 from data.prepare_data import create_rating_matrix, get_item_similarity_matrix
-from model.recommend import get_top_k_recommendations
-from constants import CHALLENGES_FILE, SUBMISSIONS_FILE
+from model.recommend import Recommendations
+from constants import CHALLENGES_FILE, SUBMISSIONS_FILE, TARGET_CONTEST_ID
 
 
 if __name__ == '__main__':
     rating_matrix, hacker_dict, challenge_dict = create_rating_matrix(CHALLENGES_FILE, SUBMISSIONS_FILE)
     similarity_matrix = get_item_similarity_matrix(rating_matrix)
-    challenges_df = pd.read_csv(CHALLENGES_FILE)
 
     print 'Getting Recommendations'
+    all_recommendations = {}
     for hacker_id in hacker_dict:
-        print get_top_k_recommendations(rating_matrix, similarity_matrix, hacker_id, hacker_dict, challenge_dict, challenges_df)
+        R = Recommendations.get_instance(TARGET_CONTEST_ID, CHALLENGES_FILE)
+        recommendations = R.get_top_k_recommendations(rating_matrix, similarity_matrix, hacker_id, hacker_dict, challenge_dict)
+        all_recommendations[hacker_id] = recommendations
+
+    with open('all_recommendations.json', 'w') as f:
+        json.dump(all_recommendations, f)
 
--- src/model/recommend.py
+++ src/model/recommend.py
@@ -1,38 +1,63 @@
 import heapq
 import numpy as np
+import pandas as pd
 
-from src.constants import TARGET_CONTEST_ID
+class Recommendations(object):
+    INSTANCE = None
+    valid_challenges = set([])
+    target_contest_id = ''
+    challenges_file = ''
 
-def get_top_k_recommendations(rating_matrix, similarity_matrix, hacker_id, hacker_dict, challenge_dict, challenges_df, k=10):
-    print 'Recommending for ' + str(hacker_id)
+    def __init__(self, target_contest_id, challenges_file):
+        Recommendations.target_contest_id = target_contest_id
+        Recommendations.challenges_file = challenges_file
+        self.find_valid_challenges()
 
-    inv_challenge_dict = {v: k for k, v in challenge_dict.iteritems()}
-    col_idx = hacker_dict[hacker_id]
-    user_col = rating_matrix.getcol(col_idx).toarray().flatten()
-    df = challenges_df
+    @classmethod
+    def find_valid_challenges(cls):
+        df = pd.read_csv(cls.challenges_file)
+        challenges = df['challenge_id'].tolist()
 
-    rated_item_indexes = [idx for idx, val in enumerate(user_col) if val == 1]
-    predicted_ratings = []
+        for c in challenges:
+            if ((df['challenge_id'] == c) & (df['contest_id'] == cls.target_contest_id)).any():
+                cls.valid_challenges.add(c)
 
-    for row_idx, val in enumerate(user_col):
-        challenge_id = inv_challenge_dict[row_idx]
+    @classmethod
+    def get_instance(cls, target_contest_id, challenges_file):
+        if not cls.INSTANCE:
+            cls.INSTANCE = Recommendations(target_contest_id, challenges_file)
+        return cls.INSTANCE
 
-        # Ignore the challenge if it is not a part of Target Contest
-        if not ((df['challenge_id'] == challenge_id) & (df['contest_id'] == TARGET_CONTEST_ID)):
-            continue
 
-        if val != 1:
-            similarities = []
-            for idx in rated_item_indexes:
-                similarities.append(similarity_matrix[row_idx, idx])
+    def get_top_k_recommendations(self, rating_matrix, similarity_matrix, hacker_id, hacker_dict, challenge_dict, k=10):
+        print 'Recommending for ' + str(hacker_id)
 
-            most_similar_items = heapq.nlargest(3, similarities)
-            predicted_ratings.append((np.mean(most_similar_items), row_idx))
+        inv_challenge_dict = {v: k for k, v in challenge_dict.iteritems()}
+        col_idx = hacker_dict[hacker_id]
+        user_col = rating_matrix.getcol(col_idx).toarray().flatten()
 
-    inv_challenge_dict = {v: k for k, v in challenge_dict.iteritems()}
+        rated_item_indexes = [idx for idx, val in enumerate(user_col) if val == 1]
+        predicted_ratings = []
 
-    recommendations = [inv_challenge_dict[row_idx] for score, row_idx in heapq.nlargest(k, predicted_ratings)]
-    return recommendations
+        for row_idx, val in enumerate(user_col):
+            challenge_id = inv_challenge_dict[row_idx]
+
+            # Ignore the challenge if it is not a part of Target Contest
+            if challenge_id not in self.valid_challenges:
+                continue 
+
+            if val != 1:
+                similarities = []
+                for idx in rated_item_indexes:
+                    similarities.append(similarity_matrix[row_idx, idx])
+
+                most_similar_items = heapq.nlargest(3, similarities)
+                predicted_ratings.append((np.mean(most_similar_items), row_idx))
+
+        inv_challenge_dict = {v: k for k, v in challenge_dict.iteritems()}
+
+        recommendations = [inv_challenge_dict[row_idx] for score, row_idx in heapq.nlargest(k, predicted_ratings)]
+        return recommendations
 
 
 
